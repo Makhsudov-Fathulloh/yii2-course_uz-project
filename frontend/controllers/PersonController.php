f4d9f989bf98c35718bf02c35b5d39dd973862c1
mr.makhsudov <mr.makhsudov@gmail.com>
2023-07-08 11:42:12 +0500

    add faker ActiveRecord

--- frontend/controllers/PersonController.php
+++ frontend/controllers/PersonController.php
@@ -2,6 +2,7 @@
 
 namespace frontend\controllers;
 
+use common\models\Person;
 use yii\web\Controller;
 use Yii;
 
@@ -9,9 +10,17 @@ class PersonController extends Controller
 {
     public function actionIndex()
     {
-        $command = Yii::$app->db->createCommand("SELECT * FROM person");
-        $result = $command->queryAll();
-
-        return $this->render('index', ['data'=>$result]);
+        return $this->render('index');
+    }
+ 
+    public function actionAdd()
+    {
+        $person = new Person();
+        $person->first_name = \Faker\Provider\en_US\Person::firstNameMale();
+        $person->last_name = \Faker\Provider\en_US\Person::firstNameMale();
+        $person->email = 'user@user.com';
+        $person->gender = \Faker\Provider\en_US\Person::GENDER_MALE;
+        $person->save(); // save() default true yani validatsiyadan otkazadi false bolsa togridan togri yozadi 
+        // shu korinishda SQL code yozmasdan person jadvaliga INSERT qilib beradi 
     }
 }
