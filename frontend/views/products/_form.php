2da4d9e132e4de1852a62fb7858799d788f98fb2
mr.makhsudov <mr.makhsudov@gmail.com>
2023-07-18 16:15:35 +0500

    create CRUD elements in Gii

---
+++ frontend/views/products/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var common\models\Products $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="products-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'productCode')->textInput() ?>
+
+    <?= $form->field($model, 'productName')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'category')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
